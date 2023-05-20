ae2728d88b5e1c32a306573c1f7d1324ea5d539a
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-21 03:39:34 +0500

    The puts method

---
+++ Section 1: Getting Started/puts.rb
@@ -0,0 +1,28 @@
+=begin
+5.times {
+  puts "Hello there!"
+}
+=end
+
+# ivoking or calling works equal
+3 + 5 # will be saved and wait for you to do with it if not it removes 😜
+puts 1
+puts 1 + 1
+puts (1 + 2) # you do not need ()
+
+puts "hello there!"
+puts "I'm alive and well!"
+
+puts 3.14159
+
+puts
+
+puts "Afterwards"
+
+puts 5
+puts 3
+puts 5
+
+puts
+
+puts 5,3,5
