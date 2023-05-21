b0536a848aa8c4ec6426937cf351bfdc765eb890
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-21 21:07:21 +0500

    Declare Variables

---
+++ Section 2: Variables/declaring_variables.rb
@@ -0,0 +1,21 @@
+age = 2023 - 2001
+puts age
+
+name = "Rustam"
+last_name = "Tolipov"
+puts name
+puts last_name
+
+puts age + 7
+puts name + " " + last_name
+
+age = 35
+puts age
+
+age_in_ten_years = age + 10
+puts age_in_ten_years
+
+chameleon = 24
+chameleon = "Some random text"
+chameleon = "3.14"
+puts chameleon
