a4386709092bb546c5d830758c4d4f0bfedd7fd9
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-21 12:15:32 +0500

    Basic Arithmetic in Ruby

---
+++ Section 1: Getting Started/basic_arithmetic_in_ruby.rb
@@ -0,0 +1,18 @@
+puts 1 + 4
+puts -6 + 13
+puts 10 - 6
+puts 3 * 4
+puts 3 ** 2 * 4
+
+# PEMDAS
+# Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
+
+puts 2 + 3 * 5
+
+puts 12 / 5
+puts 12.0 / 5
+
+# puts .5 / 2 error
+puts 0.5 / 2 # correct
+
+puts 5 % 2
