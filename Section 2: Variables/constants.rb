c96cea49d04c0b3d6d5a6ea3ec01e3e4653ac9c0
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-22 12:21:58 +0500

    Constants

---
+++ Section 2: Variables/constants.rb
@@ -0,0 +1,8 @@
+# pi = 3.14159
+PI = 3.14159 # you can use Pi
+TAX_RATE = 0.07
+
+puts PI, TAX_RATE
+
+TAX_RATE = 0.13 # constants.rb:7: warning: already initialized constant TAX_RATE
+puts TAX_RATE # constants.rb:3: warning: previous definition of TAX_RATE was here
