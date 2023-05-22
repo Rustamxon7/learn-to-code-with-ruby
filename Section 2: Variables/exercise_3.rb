5c90fb326f0a7d218f5e6d5a30478147d83dad8b
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-22 18:09:37 +0500

    Exercise 3

---
+++ Section 2: Variables/exercise_3.rb
@@ -0,0 +1,10 @@
+actor = "Stallone"
+
+# Use a method to determine the number of characters in 
+# the string above. Output the value to the screen.
+
+
+# Using the string above, invoke a method to create a new string 
+# with all characters in UPPERCASE. Output the value to the screen.
+puts actor.length
+puts actor = actor.upcase
