8c11b42e704965b36ee7bb183e877e58698c7ce1
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-22 22:34:12 +0500

    The class method

---
+++ Section 3: Object Methods/the_class_method.rb
@@ -0,0 +1,15 @@
+# Class - A blueprint for an object (template/schematic)
+# Object - An "instance" of the class
+
+puts "hello".class
+puts "Ruby is fun".class
+puts "123$".class
+puts "".class
+
+puts 
+
+puts 5.class
+
+puts 
+
+puts true.class
