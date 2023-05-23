1b7b3937902aa667246ef9e179300622b046105d
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-23 23:33:16 +0500

    Methods with Arguments (include?)

---
+++ Section 4: Booleans and Predicate Methods/methods_with_arguments.rb
@@ -0,0 +1,5 @@
+puts "Big Mac".include? "a"
+puts "Big Mac".include? "m"
+# puts "Big Mac".include? wrong number of arguments (given 0, expected 1)
+# puts "Big Mac".include? "A", "B" wrong number of arguments
+puts "Big Mac".include? ("M")
