d17835a5df6d9b7a8f4f4ebce8437d3cd84efc7e
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-24 10:19:53 +0500

    Intro to blocks

---
+++ Section 6: Blocks/introduction_to_blocks_with_the_times_method.rb
@@ -0,0 +1,11 @@
+# Argument - object, concrete value
+# Blocks - a procedure, collection of code
+
+5.times { puts "Ruby is awesome!"}
+
+value = 3.times do
+  puts "Ruby is great!"
+  puts "💎💎💎"
+end
+
+puts value
