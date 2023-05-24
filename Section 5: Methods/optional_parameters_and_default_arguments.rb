fe075692868db0684e079a82ee397ea934ce0d8e
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-24 10:13:22 +0500

    Optional Parameters

---
+++ Section 5: Methods/optional_parameters_and_default_arguments.rb
@@ -0,0 +1,8 @@
+def title_assigner(name = "Unknown name", suffix = "Unknown suffix")
+  "#{name} #{suffix}"
+end
+
+puts title_assigner
+puts title_assigner("Boris", "the Wonderful")
+puts title_assigner("Rustam")
+puts title_assigner("Max", "the Coder")
