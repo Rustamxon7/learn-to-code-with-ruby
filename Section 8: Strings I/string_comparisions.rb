2895cc684b92dfe088f33a8914a47db2817367de
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-27 15:40:50 +0500

    String Comparisions

---
+++ Section 8: Strings I/string_comparisions.rb
@@ -0,0 +1,30 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+puts a == b
+puts a == c
+puts a == a
+puts b == "hello"
+
+puts
+
+puts a != b
+puts a != c
+
+puts 
+
+puts "A" < "B"
+puts "A" < "Z"
+puts "M" < "E"
+
+puts "L" > "F"
+puts "L" > "M"
+
+puts "Z" < "a"
+puts "a" < "Z"
+
+puts "hello" < "help" # character by character
+
+# A, B, C, D, E capital comes before lowercases
+# a, b, c, d, e
