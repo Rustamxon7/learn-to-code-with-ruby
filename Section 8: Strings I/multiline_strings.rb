a9a75e1224e586b67beadd65f9e93f36aee2bbc3
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-27 22:19:35 +0500

    Multiple Strings

---
+++ Section 8: Strings I/multiline_strings.rb
@@ -0,0 +1,9 @@
+poetry = <<MLS
+  what a beautiful language Ruby is
+  It allows you to write multiple lines like this
+  Split your words up across multiple lines
+  Everything still looks nice and fine.
+    - Afandi
+MLS
+
+puts poetry
