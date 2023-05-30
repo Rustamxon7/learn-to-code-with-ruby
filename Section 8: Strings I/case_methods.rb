9ea23408b992234096990438d67829ff1268e025
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-30 10:13:50 +0500

    Case Methods

---
+++ Section 8: Strings I/case_methods.rb
@@ -0,0 +1,10 @@
+puts "Eiffel Tower".upcase
+puts "Eiffel Tower".downcase
+puts "Eiffel Tower".swapcase
+
+puts "eiffel tower".capitalize
+puts "EIFFEL TOWER".capitalize
+
+tourist_hotspot = "Eiffel Tower"
+uppercased_hotspot = tourist_hotspot.upcase
+puts tourist_hotspot
