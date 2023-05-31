07dc0d03c4919b3e746c673be6355e0d5c174559
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-31 08:07:47 +0500

    The debugger and binding

---
+++ Section 10: Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,18 @@
+require "debug"
+
+# Standard library - collection of code organized into libraries
+
+# DateTime library - helps with dates and times
+
+candy = "Sour Patch Kids"
+puts "I love eating #{candy}"
+
+debugger
+
+beverage = "Arizona Iced Tea"
+puts "I love driniking #{beverage}"
+
+debugger
+
+puts "This is the end of the program"
+
