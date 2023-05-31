ddb9f54ff16c5d79e607a925c625b7b86be3e90c
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-31 08:02:55 +0500

    Intro to arrays

---
+++ Section 11: Arrays I - Creation and Access/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+# An array is an ordered list of objects.
+#
+# We often use the word "elements" to describe the items inside the array.
+# 
+# The array itself is an object.
+# Think of a box in the real world.
+# A box can hold multiple items inside of it.
+# The box is an object and the items inside of it are objects as well.
+
+numbers = [4, 1, 5, 8, 3, 23]
+p numbers
+
+things = [4, true, "Hello", 10.99]
+p things
+
+toys = ["Teddy bear", "Water gun", "Board game"]
+
+registrations = [true, true, false, true, false]
+p registrations
