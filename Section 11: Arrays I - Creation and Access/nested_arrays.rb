430176d644028fb93836524c9a2845325d88dceb
rustamtolipov <rustamxontolipov788@gmail.com>
2023-05-31 12:21:01 +0500

    Nested arrays

---
+++ Section 11: Arrays I - Creation and Access/nested_arrays.rb
@@ -0,0 +1,16 @@
+# Column A, Column B
+# Row Value, Row Value
+
+spreadsheet = [
+  ["Student", "Class", "Grade"],
+  ["Sally", "Computer Science", 95],
+  ["Ingrid", "Physics", 45]
+]
+
+p spreadsheet
+
+first_row, second_row, thir_row = spreadsheet
+
+p first_row
+p second_row
+p thir_row
