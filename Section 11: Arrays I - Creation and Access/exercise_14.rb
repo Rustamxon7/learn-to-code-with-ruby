cbb2f6a8d29a4961f232c83813daed999cd8e869
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-19 16:24:37 +0500

    Exercise 14

---
+++ Section 11: Arrays I - Creation and Access/exercise_14.rb
@@ -0,0 +1,19 @@
+# Declare an empty_array method that returns an empty array
+
+def empty_array 
+  return []
+end
+
+# Declare a three_colors method that returns an array with
+# the strings of "red", "green", and "blue" (in that order)
+
+def three_colors 
+ return ["red", "green", "blue"]
+end
+
+# Declare a five_favorite_numbers method that returns an
+# array of 5 integers. The integers are up to you.
+
+def five_favorite_numbers
+  return [4, 5, 2, 9, 1]
+end
