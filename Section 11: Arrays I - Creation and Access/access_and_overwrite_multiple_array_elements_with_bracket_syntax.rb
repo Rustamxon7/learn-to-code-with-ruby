5d811ff7455115c1759cd404967556372581520b
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-19 15:44:09 +0500

    Access and overwrite multiple elements

---
+++ Section 11: Arrays I - Creation and Access/access_and_overwrite_multiple_array_elements_with_bracket_syntax.rb
@@ -0,0 +1,29 @@
+sesame_street = [
+  "Elmo",
+  "Big Bird",
+  "Cookie Monster",
+  "Bert",
+  "Ernie",
+  "Oscar",
+]
+
+p sesame_street[1..5]
+p sesame_street[2..4]
+p sesame_street[3..1]
+p sesame_street[3..10]
+p sesame_street[-4..-3]
+
+puts 
+
+p sesame_street.slice(0, 3)
+p sesame_street.slice(2, 4)
+p sesame_street.slice(3, 1)
+p sesame_street.slice(-4, -3)
+
+puts 
+
+sesame_street[3, 2] = ["Stinky", "Kermit"]
+p sesame_street
+
+sesame_street[3, 2] = ["Bert", "Ernie", "Julia"]
+p sesame_street
