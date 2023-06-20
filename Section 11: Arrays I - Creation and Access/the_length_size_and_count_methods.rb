316d147ec89cbe35c4026942407e49ada07b0541
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-20 18:38:49 +0500

    The length, size, and count

---
+++ Section 11: Arrays I - Creation and Access/the_length_size_and_count_methods.rb
@@ -0,0 +1,14 @@
+puts "World Hello".length
+
+puts [1, 2, 3, 4, 5].length
+puts ["Hi", "there", nil].length
+puts [].length
+puts [1, 2, 3, 4, 5].size
+puts ["Hi", "there", nil].size
+puts [].size
+
+puts 
+
+puts [1, 2, 3, 4, 5].count
+puts [1, 2, 3, 4, 5, 2].count(5)
+puts [1, 2, 3, 4, 5, 2].count(2)
