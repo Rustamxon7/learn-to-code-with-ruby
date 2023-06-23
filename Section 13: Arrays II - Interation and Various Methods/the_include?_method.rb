47a1c93fc9be6803ac0b9dbefd71a0e6ff67a090
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-23 15:05:55 +0500

    Include?

---
+++ Section 13: Arrays II - Interation and Various Methods/the_include?_method.rb
@@ -0,0 +1,9 @@
+# include? method - check for inclusion within the array
+
+p "action".include?("act")
+
+movie_genres = ["action", "comedy", "drama", "romance", "mystery"]
+
+p movie_genres.include?("action")
+p movie_genres.include?("thriller")
+p movie_genres.include?("Action")
