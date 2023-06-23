0ae0a72ab73cfad9bd4693a38bdde43e76a37382
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-23 19:37:57 +0500

    Max and min

---
+++ Section 13: Arrays II - Interation and Various Methods/the_max_and_min_methods.rb
@@ -0,0 +1,13 @@
+# max - greatest value in array
+# min - smallest value in array
+
+stock_prices = [723.99, 434.12, 84.7, 649.92]
+p stock_prices.max
+p stock_prices.min
+
+companies = ["Google", "Facebook", "Microsoft", "Apple"]
+p companies.max
+p companies.min
+
+p [].max
+p [].min
