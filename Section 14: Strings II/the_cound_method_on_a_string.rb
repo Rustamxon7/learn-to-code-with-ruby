d6dd2045af5c6f8c74ac6665c3b77a15608aa7de
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-24 13:31:07 +0500

    The count

---
+++ Section 14: Strings II/the_cound_method_on_a_string.rb
@@ -0,0 +1,7 @@
+# count method - count the number of occurences of character(s) in a string
+
+puts "Hello world".count("l")
+puts "Hello world".count("w")
+puts "Hello world".count("lo")
+puts "Hello world".count("ol")
+puts "Hello world".count("Wl")
