df989e236a2d38a14094487b06be37cc289d7b4c
rustamtolipov <rustamxontolipov788@gmail.com>
2023-06-24 20:20:58 +0500

    The join Method

---
+++ Section 14: Strings II/the_join_method_on_an_array.rb
@@ -0,0 +1,6 @@
+# join method - merge array elements into a string (optionally using a delimiter)
+
+retistrants = ["Bo", "Mo", "Joe"]
+puts retistrants.join(" - ")
+puts retistrants.join(" + ")
+puts retistrants.join(", ")
